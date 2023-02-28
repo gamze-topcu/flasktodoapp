572115a15cd6e7628a30f369671945296dbd2493
GAMZE ZEYNEP TOPCU <30399@aksa.com>
2023-02-28 09:49:39 +0300

    Flask first commit

---
+++ todo.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////code/py/ToDoApp/todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+app.app_context().push()
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    complete = db.Column(db.Boolean)
+
+
+    '''def __init__(self, title, complete):
+        self.title = title
+        self.complete = complete'''
+
+db.create_all()
+
+
+
+
+@app.route("/")
+def index():
+    todos = Todo.query.all() #todolar sözlük olarak içinde tutulur.
+    return render_template("index.html", todos = todos)
+@app.route("/delete/<string:id>")
+def deleteTodo(id):
+    todo = Todo.query.filter_by(id=id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/complete/<string:id>")
+def completeTodo(id):
+    todo = Todo.query.filter_by(id = id).first()
+    todo.complete = not todo.complete
+    db.session.commit() #değişiklik yaptığımız için
+    return redirect(url_for("index"))
+@app.route("/add", methods = ["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+'''''''''@app.route("/add", methods = ["POST","GET"])
+def addTodo():
+    if request.method == "POST":
+        title = request.form.get("title")
+        newTodo = Todo(title=title, complete=False)
+        db.session.add(newTodo)
+        db.session.commit()
+        return redirect(url_for("index"))
+    else:
+        return redirect(url_for("index"))'''''
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
